56d873a7ca867ac00462a51988da8c9f67dd5fbe
Serhii Kyslovets <serhii.love@gmail.com>
2020-05-16 17:40:09 +0300

    chore: update tours API handler

--- natours/app.js
+++ natours/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
 const app = express();
+const fs = require("fs");
+
+const tours = JSON.parse(
+	fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get("/api/v1/tours", (req, res) => {
-	res.send("Tours");
+	res.json(tours);
 });
 
 const port = 3000;
