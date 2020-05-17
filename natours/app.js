9a4a326e455873af90fbe5537670c98210984adb
Serhii Kyslovets <serhii.love@gmail.com>
2020-05-17 15:47:22 +0300

    feat: new tour API handler created

--- natours/app.js
+++ natours/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
+app.use(express.json());
+
 const tours = JSON.parse(
 	fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -14,6 +16,27 @@ app.get('/api/v1/tours', (req, res) => {
 			tours: tours,
 		},
 	});
+});
+
+app.post('/api/v1/tours', (req, res) => {
+	const newId = tours[tours.length - 1].id + 1;
+	const newTour = Object.assign({ id: newId }, req.body);
+
+	tours.push(newTour);
+	fs.writeFile(
+		`${__dirname}/dev-data/data/tours-simple.json`,
+		JSON.stringify(tours),
+		err => {
+			if (err) throw err;
+			
+			res.status(201).json({
+				"status": "success",
+				"data": {
+					"tour": newTour
+				}
+			});
+		}
+	);
 });
 
 const port = 3000;
