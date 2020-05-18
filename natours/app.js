0414a9aad00dc7daf366b6ff4e459bac3af4eae6
Serhii Kyslovets <serhii.love@gmail.com>
2020-05-18 11:07:21 +0300

    chore: add patch tour API handler

--- natours/app.js
+++ natours/app.js
@@ -37,6 +37,20 @@ app.get('/api/v1/tours/:id', (req, res) => {
 	});
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+	if (req.params.id * 1 > tours.length) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID'
+		});
+	}
+
+	res.status(200).json({
+		status: 'success',
+		tour: '<Updated tour here ...>'
+	})
+});
+
 app.post('/api/v1/tours', (req, res) => {
 	const newId = tours[tours.length - 1].id + 1;
 	const newTour = Object.assign({ id: newId }, req.body);
