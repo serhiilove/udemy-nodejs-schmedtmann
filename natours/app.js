0f286f04647955dadeccfc401295669193ecd64f
Serhii Kyslovets <serhii.love@gmail.com>
2020-05-18 10:38:35 +0300

    chore: add paramenet parsing to the get tours API handler

--- natours/app.js
+++ natours/app.js
@@ -18,6 +18,25 @@ app.get('/api/v1/tours', (req, res) => {
 	});
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+	const id = req.params.id * 1;
+	const tour = tours.find((obj) => obj.id === id);
+
+	if (!tour) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID'
+		});
+	}
+	
+	res.status(200).json({
+		status: 'success',
+		data: {
+			tour: tour,
+		},
+	});
+});
+
 app.post('/api/v1/tours', (req, res) => {
 	const newId = tours[tours.length - 1].id + 1;
 	const newTour = Object.assign({ id: newId }, req.body);
@@ -26,14 +45,14 @@ app.post('/api/v1/tours', (req, res) => {
 	fs.writeFile(
 		`${__dirname}/dev-data/data/tours-simple.json`,
 		JSON.stringify(tours),
-		err => {
+		(err) => {
 			if (err) throw err;
-			
+
 			res.status(201).json({
-				"status": "success",
-				"data": {
-					"tour": newTour
-				}
+				status: 'success',
+				data: {
+					tour: newTour,
+				},
 			});
 		}
 	);
