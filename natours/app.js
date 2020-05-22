4a93ddeb95a002cd7b3d3531da44f468ac5f7b2b
Serhii Kyslovets <serhii.love@gmail.com>
2020-05-22 21:29:36 +0300

    chore: update users API handlers

--- natours/app.js
+++ natours/app.js
@@ -108,19 +108,31 @@ const getAllUsers = (req, res) => {
 };
 
 const createUser = (req, res) => {
-	//
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet defined'
+	});
 };
 
 const getUser = (req, res) => {
-	// 
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet defined'
+	});
 }
 
 const updateUser = (req, res) => {
-	// 
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet defined'
+	});
 }
 
 const deleteUser = (req, res) => {
-	// 
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet defined'
+	});
 }
 
 // TOURS ROUTES
