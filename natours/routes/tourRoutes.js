81613fbd4d7b8752cc1c6cb4d1230ad0190509b6
Serhii Kyslovets <serhii.love@gmail.com>
2020-05-24 23:07:37 +0300

    chore: add param id log

--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -3,6 +3,11 @@ const tourController = require('../controllers/tourController');
 
 const router = new express.Router();
 
+router.param('id', (req, res, next, val) => {
+	console.log(`Tour param is: ${val}`);
+	next();
+});
+
 router
 	.route('/')
 	.get(tourController.getAllTours)
