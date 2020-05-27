f3708ddeda09c84b3239c5483b676dfb179c9b7d
Serhii Kyslovets <serhii.love@gmail.com>
2020-05-27 12:47:23 +0300

    chore: server updated

--- natours/server.js
+++ natours/server.js
@@ -1,8 +1,8 @@
-const app = require('./app');
 const dotenv = require('dotenv');
-
 dotenv.config({ path: './config.env'});
 console.log(process.env);
+
+const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
