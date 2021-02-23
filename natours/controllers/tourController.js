4f9c05adba60ed03730006dd3ff9ec39e799d8fc
Serhii Kyslovets <serhii.love@gmail.com>
2021-02-23 10:58:10 +0200

    chore: add field limiting

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -24,14 +24,13 @@ exports.getAllTours = async (req, res) => {
 			query = query.sort('-createAt');
 		}
 
-		// Sorting
-		// if (req.query.sort) {
-		// 	const sortBy = req.query.sort.split(',').join(' ');
-		// 	console.log('sortBy: ', sortBy);
-		// 	query = query.sort(sortBy);
-		// } else {
-		// 	query = query.sort('createdAt');
-		// }
+		// 3) Field limiting
+		if (req.query.fields) {
+			const fields = req.query.fields.split(',').join(' ');
+			query = query.select(fields);
+		} else {
+			query = query.select('-__v');
+		}
 
 		const tours = await query;
 
