06a7c467a7c929318ec06a632ca356f1152c5044
Serhii Kyslovets <serhii.love@gmail.com>
2021-02-23 10:26:11 +0200

    chore: add tours sorting

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -2,7 +2,38 @@ const Tour = require('../models/toursModel');
 
 exports.getAllTours = async (req, res) => {
 	try {
-		const tours = await Tour.find();
+		// 1A) Filtering
+		const queryObj = { ...req.query };
+		const excludedFields = ['page', 'sort', 'limit', 'fields'];
+		excludedFields.forEach((el) => delete queryObj[el]);
+
+		// 1B) Advanced filtering
+		let queryStr = JSON.stringify(queryObj);
+		queryStr = queryStr.replace(
+			/\b(gte|gt|lte|lt)\b/g,
+			(match) => `$${match}`
+		);
+
+		let query = Tour.find(JSON.parse(queryStr));
+
+		// 2) Sorting
+		if (req.query.sort) {
+			const sortBy = req.query.sort.split(',').join(' ');
+			query = query.sort(sortBy);
+		} else {
+			query = query.sort('-createAt');
+		}
+
+		// Sorting
+		// if (req.query.sort) {
+		// 	const sortBy = req.query.sort.split(',').join(' ');
+		// 	console.log('sortBy: ', sortBy);
+		// 	query = query.sort(sortBy);
+		// } else {
+		// 	query = query.sort('createdAt');
+		// }
+
+		const tours = await query;
 
 		res.status(200).json({
 			status: 'success',
--- natours/server.js
+++ natours/server.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+const app = require('./app');
+
 console.log(process.env.NODE_ENV);
 
 const DB = process.env.DATABASE.replace(
